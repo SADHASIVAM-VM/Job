197262d431ba6990cc1725b3efad666c76d3f96c
SADHASIVAM-VM <sadha4747@gmail.com>
2024-05-01 10:58:16 +0530

    ched

--- src/Backend/Server.js
+++ src/Backend/Server.js
@@ -4,17 +4,12 @@ const JsData =require('../db.json')
 
 const app = express();
 
-const allowedOrigins = ['https://splendorous-choux-bc8ab0.netlify.app','https://splendorous-choux-bc8ab0.netlify.app/','splendorous-choux-bc8ab0.netlify.app/:1'];
-app.use(cors({
-  origin: function (origin, callback) {
-    // Check if the origin is allowed or is undefined (for server-to-server requests)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+const allowedOrigins = ['https://splendorous-choux-bc8ab0.netlify.app/','splendorous-choux-bc8ab0.netlify.app/:1'];
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://splendorous-choux-bc8ab0.netlify.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 app.get('/Jobs', (req, res) => {
