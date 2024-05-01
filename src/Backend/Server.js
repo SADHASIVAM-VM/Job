e7bc40834966e5cf6fffd20193469041d7b52c1b
SADHASIVAM-VM <sadha4747@gmail.com>
2024-05-01 08:25:36 +0530

    ch

--- src/Backend/Server.js
+++ src/Backend/Server.js
@@ -6,7 +6,7 @@ const Port = process.env.PORT || 3000
 app.use((req, res, next) => {
     const allowedOrigins = ['http://localhost:5174',"https://splendorous-choux-bc8ab0.netlify.app"]; 
     const origin = req.headers.origin;
-    if (allowedOrigins === origin) {
+    if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
