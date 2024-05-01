0cb6949c28b68ca63cd20b4a685660a5cf6f86bf
SADHASIVAM-VM <sadha4747@gmail.com>
2024-05-01 08:37:43 +0530

    ch

--- src/Backend/Server.js
+++ src/Backend/Server.js
@@ -1,22 +1,28 @@
-const express = require('express')
-const app = express()
-const jsData = require('../db.json')
-const cors = require('cors') 
-const Port = process.env.PORT || 3000
+const express = require('express');
+const cors = require('cors');
+const JsData =require('../db.json')
 
-const allowedOrigins = ['http://localhost:5174',"https://splendorous-choux-bc8ab0.netlify.app"]; 
+const app = express();
+
+const allowedOrigins = ['https://splendorous-choux-bc8ab0.netlify.app','https://splendorous-choux-bc8ab0.netlify.app'];
 app.use(cors({
-    origin:function (origin, callback){
-        if(!origin || allowedOrigins.includes(origin)){
-            callback(null, true);
-        }else{
-            callback(new Error("Not Allowed By Cors"))
-        }
+  origin: function (origin, callback) {
+    // Check if the origin is allowed or is undefined (for server-to-server requests)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
+  }
 }));
-app.get('/Jobs',(req,res)=>{
-    res.json(jsData)
-})
-app.listen(Port,()=>{
-console.log('Server is Working',Port)
-})
+
+
+app.get('/Jobs', (req, res) => {
+   res.send()
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
